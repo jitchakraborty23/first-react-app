78663e3b22aaff2c784e2cefe3aaa57da937ff3b
jitchakraborty23 <cjit2307@gmail.com>
2023-05-05 23:10:13 +0530

    first react app created

---
+++ src/DemoLogin.js
@@ -0,0 +1,74 @@
+import {Button,Col,Row,Form,Container} from 'react-bootstrap';
+import "./DemoLogin.css";
+function DemoLogin()
+{
+    return (
+     <>
+     <center>
+ 
+      <div className='login1'>
+      <div className='login'>
+    <Form >
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+        <Form.Label column sm={2}>
+          Email
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control type="email" placeholder="Email" className='
+          -placeholder'/>
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+        <Form.Label column sm={2}>
+          Password
+        </Form.Label>
+         <Col sm={10}>
+          <Form.Control type="password" placeholder="Password" />
+        </Col>
+      </Form.Group>
+      {/* <fieldset>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label as="legend" column sm={2}>
+            Radios
+          </Form.Label> */}
+          {/* <Col sm={10}> */}
+            {/* <Form.Check
+              type="radio"
+              label="first radio"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios1"
+            />
+            <Form.Check
+              type="radio"
+              label="second radio"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios2"
+            />
+            <Form.Check
+              type="radio"
+              label="third radio"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios3"
+            /> */}
+          {/* </Col> */}
+        {/* </Form.Group>
+      </fieldset>  */}
+      <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+        <Col sm={{ span: 10, offset: 2 }}>
+          <Form.Check label="Remember me" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3">
+        <Col sm={{ span: 10, offset: 2 }}>
+          <Button type="submit">Sign in</Button>
+        </Col>
+      </Form.Group>
+    </Form></div>
+    </div>
+    </center>
+
+       </>);
+}
+export default DemoLogin;
